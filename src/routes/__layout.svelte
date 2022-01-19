e21ea20c92badd9277dc12af5aa2a036e869d7f3
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2022-01-19 11:33:23 -0500

    Diseño

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,39 @@
+<script>
+	import { onMount } from 'svelte';
+	import { loadJS } from '$lib/store';
+	onMount(async () => {
+		loadJS('/js/bootstrap.min.js', () => {})
+	})
+</script>
+
 <svelte:head>
 	<title>Testigo Electoral</title>
 </svelte:head>
-<h1>Testigo Electoral</h1>
-<slot />
+
+<header class="py-3 mt-2 mb-2 ms-2 me-2">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-lg rounded">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarMenu">
+				<a class="navbar-brand" href="#">TE</a>
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">Inicio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="/">Subir foto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<div class="row bg-white text-dark shadow-lg bg-body rounded mt-2 mb-5 ms-2 me-2">
+	<div class="col pt-3 pb-3">
+		<h1 class="text-center mb-3">Testigo Electoral</h1>
+		<slot />
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,84 @@
 <script>
 	import { signIn, getProfile, signOut } from '$lib/auth';
 
-	let profile = getProfile()
+	let profile = getProfile();
 
 	function clickLogout() {
 		profile = signOut();
 	}
-	
 </script>
 
-{#await profile}
-	<p>...waiting</p>
-{:then user}	
-	{#if !user.name}
-		<button on:click={signIn}>Ingresar/Registrarse</button>
-	{:else}
-		<h2>Bienvenido {user.name}!</h2>
-		<p>Apareces identificado con CC{user.username}</p>
-		<p>Tu email ({user.email}) ha sido verificado: {user.email_verified}</p>
-		<p>Tu telefono ({user.phone_number}) ha sido verificado: {user.phone_number_verified}</p>
-		<p>Tu hash es: {user['custom:hash']}</p>
-		<button on:click={clickLogout}>Salir</button>
-	{/if}
-{:catch error}
-	<p style="color: red">{error.message}</p>
-{/await}
+<div class="row justify-content-md-center">
+	<div class="col-md-6">
+		{#await profile}
+			<div class="d-flex justify-content-center">
+				<div class="spinner-border" role="status">
+					<span class="visually-hidden">Cargando...</span>
+				</div>
+			</div>
+		{:then user}
+			{#if !user.name}
+				<div class="card card-cover h-100 overflow-hidden rounded-5 shadow-lg">
+					<div class="card-body">
+						<h5 class="card-title">Iniciar Sesión</h5>
+						<p class="card-text">Tengo unas palabras para decir pero no me animo.</p>
+						<div class="d-grid gap-2">
+							<button href="#" class="btn btn-primary" on:click={signIn}>
+								Ingresar/Registrarse
+							</button>
+						</div>
+					</div>
+				</div>
+			{:else}
+				<div class="card card-cover h-100 overflow-hidden rounded-5 shadow-lg">
+					<div class="card-body">
+						<h5 class="card-title">Bienvenido {user.name}!</h5>
+
+						<ul class="list-group">
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto text-truncate">
+									<div class="fw-bold">Apareces identificado con</div>
+									CC {user['cognito:username']}
+								</div>
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto text-truncate">
+									<div class="fw-bold">Tu email</div>
+									{user.email}
+								</div>
+								{#if user.email_verified}
+									<span class="badge bg-success rounded-pill">OK</span>
+								{:else}
+									<span class="badge bg-danger rounded-pill">NO</span>
+								{/if}
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto text-truncate">
+									<div class="fw-bold">Tu telefono</div>
+									{user.phone_number}
+								</div>
+								{#if user.phone_number_verified}
+									<span class="badge bg-success rounded-pill">OK</span>
+								{:else}
+									<span class="badge bg-danger rounded-pill">NO</span>
+								{/if}
+							</li>
+							<li class="list-group-item d-flex justify-content-between align-items-start">
+								<div class="ms-2 me-auto text-truncate">
+									<div class="fw-bold">Tu hash es</div>
+									{user['custom:hash']}
+								</div>
+							</li>
+						</ul>
+
+						<div class="d-grid gap-2 mt-3">
+							<button href="#" class="btn btn-primary" on:click={clickLogout}> Salir </button>
+						</div>
+					</div>
+				</div>
+			{/if}
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	</div>
+</div>
