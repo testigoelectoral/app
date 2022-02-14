5c2a1c4de263c7d36b9e52b8332df31b1c078188
Andres Herrera <andres.herrera@gmail.com>
2022-02-14 02:18:48 -0300

    Moviendo funciones de GeoLocation a componente

--- src/routes/upload.svelte
+++ src/routes/upload.svelte
@@ -2,47 +2,20 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { getProfile, user, toLogin } from '$lib/account';
-	import { loadJS } from '$lib/store';
 	import QrScanner from 'qr-scanner';
 	import { puestoFromQR } from '$lib/data'
 	import Api from '$lib/api'
+	import GeoLocation from '$lib/GeoLocation.svelte'
 
-	let errorMsg, location;
+	let location;
 	let api;
-
 
 	onMount(async () => {
 		await getProfile();
 		if (!$user) toLogin();
-
 		api = new Api();
-		let geoModal, id;
-		let options = {
-			enableHighAccuracy: false,
-			timeout: 5000,
-			maximumAge: 0
-		};
-		function error(err) {
-			errorMsg = err.message;
-			geoModal.show();
-		}
-
-		function success(pos) {
-			location = pos.coords;
-		}
-		function checkGPS() {
-			id = navigator.geolocation.watchPosition(success, error, options);
-		}
-
-		loadJS('/js/bootstrap.min.js', () => {
-			let xid = document.getElementById('geoModal');
-			geoModal = new bootstrap.Modal(xid);
-			xid.addEventListener('hidden.bs.modal', function (event) {
-				checkGPS();
-			});
-			checkGPS();
-		});
 	});
+
 	const preview = writable([]);
 	let files = [];
 	let filesQr = {};
@@ -92,35 +65,7 @@
 	<title>Subir Imágenes</title>
 </svelte:head>
 
-<div
-	class="modal fade"
-	id="geoModal"
-	tabindex="-1"
-	aria-labelledby="geoModalLabel"
-	aria-hidden="true"
->
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title" id="geoModalLabel">Necesito permisos</h5>
-				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" />
-			</div>
-			<div class="modal-body">
-				<div class="alert alert-danger mt-2" role="alert">
-					{errorMsg}
-				</div>
-
-				<div class="alert alert-warning mt-2" role="alert">
-					<i class="bi bi-exclamation-triangle" />
-					Debe permitir el uso de GPS para este paso
-				</div>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-			</div>
-		</div>
-	</div>
-</div>
+<GeoLocation bind:coords={location}/>
 
 {#if $user}
 	<div class="card card-cover overflow-hidden rounded-5 shadow-lg mt-2">
