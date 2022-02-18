bf8d8d374c651c0f09d0671615e1f380ae4cb0bd
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2022-02-18 09:17:27 -0500

    arreglo de ese mierdero (#21)
    * arreglo de ese mierdero
    
    * faltaba un link
    
    * faltaba un color

--- src/routes/myimages.svelte
+++ src/routes/myimages.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onMount } from 'svelte';
 	import { getProfile, user, toLogin } from '$lib/account';
-	import Api from '$lib/api'
+	import Api from '$lib/api';
 
 	let promise = Promise.resolve([]);
 	let api;
@@ -27,15 +27,25 @@
 					Cargando lista de imágenes ...
 				{:then images}
 					{#each images as image}
-						<div class="mt-2 me-2">
-							<a href="/image/{image.ImageID}" class="link-dark text-center m-2 py-3 bg-light rounded">
-								<img class="img-fluid" alt="img" src="{image.url}" />
+						<div
+							class="mt-2 me-2 px-2 py-2 border-4 rounded border {image.OwnerReport
+								? 'border-warning'
+								: 'border-success'} pr"
+						>
+							<img class="img-fluid" alt="img" src={image.url} />
+							<div class="d-grid mt-2 pa b0">
 								{#if !image.OwnerReport}
-									<div class="mt-2">Votar</div>
+									<a class="btn btn-success" href="/image/{image.ImageID}">
+										<i class="bi bi-archive" />
+										Votar
+									</a>
 								{:else}
-									<div class="mt-2">Revisar</div>
+									<a class="btn btn-warning" href="/image/{image.ImageID}">
+										<i class="bi bi-archive" />
+										Revisar
+									</a>
 								{/if}
-							</a>
+							</div>
 						</div>
 					{/each}
 				{:catch error}
@@ -47,7 +57,18 @@
 {/if}
 
 <style lang="scss">
+	.pr {
+		position: relative;
+	}
+	.pa {
+		position: absolute;
+	}
+	.b0 {
+		bottom: 8px;
+		width: calc(100% - 16px);
+	}
 	.img-fluid {
-		max-width: 150px;
+		max-width: 138px;
+		margin-bottom: 46px;
 	}
 </style>
